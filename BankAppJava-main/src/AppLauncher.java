1f22041589985f1df2c24e0b5ed744c7785f8f65
Ercan Çam <camercan30@gmail.com>
2025-04-30 12:56:50 +0300

    Initial commit

---
+++ BankAppJava-main/src/AppLauncher.java
@@ -0,0 +1,18 @@
+import db_objs.User;
+import guis.BankingAppGui;
+import guis.LoginGui;
+import guis.RegisterGui;
+
+import javax.swing.*;
+import java.math.BigDecimal;
+
+public class AppLauncher {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable(){
+            @Override
+            public void run() {
+                new LoginGui().setVisible(true);
+            }
+        });
+    }
+}
